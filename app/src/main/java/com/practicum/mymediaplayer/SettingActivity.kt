e37ccf06d377725a69ccbbc6ca5ea4bb2ff7c206
Practicum <practicum@ya.ru>
2023-02-17 15:35:31 +0100

    MyMediaPlayer Sprint9 homework (include Styles,SwitchCompat and DarkMode)

--- app/src/main/java/com/practicum/mymediaplayer/SettingActivity.kt
+++ app/src/main/java/com/practicum/mymediaplayer/SettingActivity.kt
@@ -1,11 +1,13 @@
 package com.practicum.mymediaplayer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 
 class SettingActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
