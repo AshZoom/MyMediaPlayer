8bccfe598d33872606a9e5ca73a430774042ab24
Practicum <practicum@ya.ru>
2023-02-21 12:12:09 +0100

    MyMediaPlayer Sprint9 homework (include Styles,SwitchCompat,DarkMode,Strings (en and ru), and Clicker on buttons: share_app,support and user_agreement)

--- app/src/main/java/com/practicum/mymediaplayer/SettingActivity.kt
+++ app/src/main/java/com/practicum/mymediaplayer/SettingActivity.kt
@@ -1,9 +1,12 @@
 package com.practicum.mymediaplayer
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class SettingActivity : AppCompatActivity() {
@@ -13,6 +16,34 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
         findViewById<Button>(R.id.button_arrow_back).setOnClickListener { finish() }
 
+        findViewById<ImageView>(R.id.button_share_app).setOnClickListener {
+            val message="https://practicum.yandex.ru/profile/android-developer/"
+            val shareIntent= Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT,message)
+            shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+
+        }
+
+        findViewById<ImageView>(R.id.button_support).setOnClickListener {
+            val message="Спасибо разработчикам и разработчицам за крутое приложение!"
+            val messagetheme="Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val shareIntent= Intent(Intent.ACTION_SENDTO)
+            shareIntent.data= Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("alexey-shmakov@yandex.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT,message)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT,messagetheme)
+            //shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+
+        }
+
+        findViewById<ImageView>(R.id.button_user_agreement).setOnClickListener {
+            val address=Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val shareIntent= Intent(Intent.ACTION_VIEW,address)
+            startActivity(shareIntent)
+
+        }
 
     }
 }
