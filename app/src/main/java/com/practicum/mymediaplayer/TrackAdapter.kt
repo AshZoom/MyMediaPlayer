47d32e207d5e4c304a901878148810b97b5eb712
Practicum <practicum@ya.ru>
2023-03-18 17:01:14 +0300

    Sprint 11 HomeWork initial

--- app/src/main/java/com/practicum/mymediaplayer/TrackAdapter.kt
+++ app/src/main/java/com/practicum/mymediaplayer/TrackAdapter.kt
@@ -11,51 +11,53 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.module.LibraryGlideModule
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 
-class TrackAdapter(private var tracks: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.song_list_layout, parent, false)
-        return TrackViewHolder(view)
-    }
+    var tracks = ArrayList<Track>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder =
+        TrackViewHolder(parent)
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        holder.bind(tracks[position])
+        holder.bind(tracks.get(position))
     }
 
-    override fun getItemCount(): Int {
-        return tracks.size
-    }
+    override fun getItemCount(): Int = tracks.size
 
 }
 
-class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(parent: ViewGroup) :
+    RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.song_list_layout, parent, false)
+    ) {
 
-    private val imageViewHolder: ImageView
-    private val trackNameView: TextView
-    private val artistNameView: TextView
-    private val trackTimeView: TextView
-
-    init {
-        imageViewHolder = itemView.findViewById(R.id.image)
-        trackNameView = itemView.findViewById(R.id.track_name)
-        artistNameView = itemView.findViewById(R.id.artist_name)
-        trackTimeView = itemView.findViewById(R.id.track_time)
-    }
+    var imageViewHolder: ImageView = itemView.findViewById(R.id.image)
+    var trackNameView: TextView = itemView.findViewById(R.id.track_name)
+    var artistNameView: TextView = itemView.findViewById(R.id.artist_name)
+    var trackTimeView: TextView = itemView.findViewById(R.id.track_time)
 
     fun bind(model: Track) {
-        trackNameView.text = model.trackName
-        artistNameView.text = model.artistName
-        trackTimeView.text = model.trackTime
+
         Glide.with(itemView)
             .load(model.artworkUrl100)
             .transform(
-                RoundedCorners(itemView.resources
-                .getDimensionPixelSize(R.dimen.setting_rounded_Corners)))
+                RoundedCorners(
+                    itemView.resources
+                        .getDimensionPixelSize(R.dimen.setting_rounded_Corners)
+                )
+            )
             .placeholder(R.mipmap.ic_vector)
             .into(imageViewHolder)
+
+        trackNameView.text = model.trackName
+        artistNameView.text = model.artistName
+        trackTimeView.text =
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis)
 
     }
 }
